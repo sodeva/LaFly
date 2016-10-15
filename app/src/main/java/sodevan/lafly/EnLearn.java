a1b9e99d160a00103bee27f635a5e063cd0a449c
kartiksharma <98kartik.sharma@gmail.com>
2016-10-15 22:28:25 +0530

    enlearn

--- app/src/main/java/sodevan/lafly/EnLearn.java
+++ app/src/main/java/sodevan/lafly/EnLearn.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,22 +13,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerSupportFragment;
-import com.google.android.youtube.player.YouTubeThumbnailLoader;
-import com.google.android.youtube.player.YouTubeThumbnailView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.pierfrancescosoffritti.youtubeplayer.AbstractYouTubeListener;
+import com.pierfrancescosoffritti.youtubeplayer.YouTubePlayerView;
 
-    public class EnLearn  extends Fragment {
+public class EnLearn  extends Fragment {
     private ListView lv;
     Typeface roboto;
     FirebaseDatabase database;
     DatabaseReference ref;
-    private YouTubePlayer YPlayer;
     Context c ;
-        YouTubePlayerSupportFragment youTubePlayerFragment ;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -35,6 +31,44 @@ import com.google.firebase.database.FirebaseDatabase;
 
         View v = inflater.inflate(R.layout.activity_en_learn ,container  , false) ;
 
+        database = FirebaseDatabase.getInstance() ;
+        ref = database.getReference("Enlearn Videos")  ;
+        lv = (ListView) v.findViewById(R.id.enlearnlistview);
+
+
+        FirebaseListAdapter<VideoChild> fb = new FirebaseListAdapter<VideoChild>(getActivity(), VideoChild.class, R.layout.videochild, ref) {
+
+            @Override
+            protected void populateView(View v, VideoChild model, int position) {
+                roboto=Typeface.createFromAsset(c.getAssets() , "roboto.ttf") ;
+              final String url=model.getURL();
+                String date=model.getDate();
+                String name=model.getName();
+                TextView Date=(TextView)v.findViewById(R.id.date);
+                TextView Name=(TextView)v.findViewById(R.id.heading);
+                Name.setTypeface(roboto);
+                Date.setTypeface(roboto);
+                Name.setText(name);
+                Date.setText(date);
+                final YouTubePlayerView youTubePlayerView = (YouTubePlayerView)v. findViewById(R.id.youtube_player_view);
+                youTubePlayerView.initialize(new AbstractYouTubeListener() {
+                    @Override
+                    public void onReady() {
+                        youTubePlayerView.loadVideo(url, 0);
+                    }
+                }, true);
+                youTubePlayerView.setCustomActionRight(ContextCompat.getDrawable(c, R.drawable.ic_pause_36dp), new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                    }
+                });
+
+
+            }
+
+
+        };
+        lv.setAdapter(fb);
         return v ;
 
     }
