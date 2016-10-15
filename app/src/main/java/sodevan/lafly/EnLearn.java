83eee34c681fc8d406313712c249bb05df906974
kartiksharma <98kartik.sharma@gmail.com>
2016-10-15 22:36:21 +0530

    enlearn

--- app/src/main/java/sodevan/lafly/EnLearn.java
+++ app/src/main/java/sodevan/lafly/EnLearn.java
@@ -5,7 +5,6 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -57,12 +56,6 @@ public class EnLearn  extends Fragment {
                         youTubePlayerView.loadVideo(url, 0);
                     }
                 }, true);
-                youTubePlayerView.setCustomActionRight(ContextCompat.getDrawable(c, R.drawable.ic_pause_36dp), new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                    }
-                });
-
 
             }
 
